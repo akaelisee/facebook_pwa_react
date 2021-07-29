20a98e9147e2cebe58da8c31bd4fc607a1b500c9
AKA Jean-Elisée <jean-elisee.aka@isoskele.fr>
2021-07-30 00:11:41 +0200

    create folder register view

---
+++ src/views/register.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Container } from '../styles/Container'
+
+const Register = props => {
+  return (
+      <Container>
+        <span> register </span>
+      </Container>
+  )
+}
+
+Register.propTypes = {
+
+}
+
+export default Register
