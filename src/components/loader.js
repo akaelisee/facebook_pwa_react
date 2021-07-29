4c22d3888b3cec03edd5e6b2dae9fe885307b0b0
AKA Jean-Elisée <jean-elisee.aka@isoskele.fr>
2021-07-30 00:09:22 +0200

    reorganisation

---
+++ src/components/loader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container } from '../styles/Container'
+import { PageFixed, LoaderPage } from '../styles/Loader'
+
+const Loader = () => {
+  return (
+    <Container>
+      <PageFixed>
+        <LoaderPage></LoaderPage>
+      </PageFixed>
+    </Container>
+  )
+}
+
+export default Loader
